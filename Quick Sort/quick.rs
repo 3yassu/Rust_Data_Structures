663203ca25ed8de905c45b857fc0cb6e73e3b555
3yassu <68251430+3yassu@users.noreply.github.com>
2025-03-17 23:14:19 -0500

    Error - edited queue and commited quick... fixed

--- Quick Sort/quick.rs
+++ Quick Sort/quick.rs
@@ -1,25 +1,18 @@
-use std::cmp::Ordering;
-fn partition<T: Ord>(&mut part_list: Vec<T>,> low: usize, high: usize) -> Vec<T>{
-    let max = part_list.len() - 1;
-    let pivot = part_list[max];
-    let left = low - 1;
-    for i in 0..max {
-        if part_list[i] <= pivot{
+fn partition<T: Ord>(part_list: &mut Vec<T>, low: usize, high: usize) -> usize{
+    let mut left = low;
+    for i in low..high {
+        if part_list[i] <= part_list[high]{
+            part_list.swap(left, i);
             left += 1;
-            let temp = part_list[i];
-            part_list[i] = part_list[left];
-            part_list[left] = temp;
-        }{
-            let temp = part_list[max];
-            part_list[max] = part_list[left];
-            part_list[left] = temp;
         }
-        left + 1
     }
+    part_list.swap(left, high);
+    return left;
 }
-fn quick_sort<T: Ord>(&mut quick_list: Vec<T>, low: usize, high: usize){
+fn quick_sort<T: Ord>(quick_list: &mut Vec<T>, low: usize, high: usize){
     if low < high {
-        part = partition(&mut quick_list, low, high);
-        quick_sort(quick_list, low, part - 1); quick_sort(quick_list, part + 1, high);
+        let part = partition(quick_list, low, high);
+        if part > 0 {quick_sort(quick_list, low, part - 1);}
+        quick_sort(quick_list, part + 1, high);
     }
 }
