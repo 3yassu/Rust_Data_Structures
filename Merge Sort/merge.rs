b1d0e7aa387ce42f699c43d9549b280f4e57d5b9
3yassu <68251430+3yassu@users.noreply.github.com>
2025-03-17 21:43:26 -0500

    Update merge.rs - Continue working on merge sort
    Forgot to put in the function return type, will begin testing current implementation

--- Merge Sort/merge.rs
+++ Merge Sort/merge.rs
@@ -1,4 +1,4 @@
-fn merge(list_1: Vec<T: Ord>, list_2: Vec<T: Ord>){
+fn merge(list_1: Vec<T: Ord>, list_2: Vec<T: Ord>) -> Vec<T: Ord>{
     let mut return_list = Vec::new();
     while list_1 || list_2 {
         if list_1.len() == 0 {return_list.push(list_2.remove(0));}
@@ -13,6 +13,8 @@ fn merge(list_1: Vec<T: Ord>, list_2: Vec<T: Ord>){
     }
     return_list
 }
-fn merge_sort(merge_list: Vec<T: Ord>){
-    merge_list.split_off(merge_list.len()/2)
+fn merge_sort(merge_list: Vec<T: Ord>) -> Vec<T: Ord>{
+    if merge_list.len() == 1 {merge_list}
+    let mut recurse_list = merge_list.split_off(merge_list.len()/2);
+    merge(merge_sort(merge_list), merge_sort(recurse_list))
 }
