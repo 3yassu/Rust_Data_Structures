efd76bd159dae137f7b00d74f176bacf419372a5
3yassu <68251430+3yassu@users.noreply.github.com>
2025-03-17 21:36:03 -0500

    Upload merge.rs - Begin working on merge sort

---
+++ Merge Sort/merge.rs
@@ -0,0 +1,15 @@
+fn merge(list_1: Vec<T: Ord>, list_2: Vec<T: Ord>){
+    let mut return_list = Vec::new();
+    while list_1 || list_2 {
+        if list_1.len() == 0 {return_list.push(list_2.remove(0));}
+        else if list_2.len() == 0 {return_list.push(list_1.remove(0));}
+        else{ 
+            match list_1[0].cmp(&list_2[0]){
+                Ordering::Less => return_list.push(list_1.remove(0)),
+                Ordering::Equal => {return_list.push(list_1.remove(0)); list_2.remove(0)},
+                Ordering::Greater => return_list.push(list_2.remove(0))
+            }
+        }
+    }
+    return_list
+}
