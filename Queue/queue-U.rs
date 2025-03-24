e907d6d628ff5dfe364a881d24f31854e4ba10ad
3yassu <68251430+3yassu@users.noreply.github.com>
2025-03-24 01:24:20 -0500

    Update queue-U.rs - Implement Iterator
    Will continue to add more functionality later

--- Queue/queue-U.rs
+++ Queue/queue-U.rs
@@ -1,21 +1,21 @@
 use core::ptr::NonNull;
 #[derive(Debug)]
-struct Node<T: Ord>{
+struct Node<T>{
     entry: T,
     next: NodeChild<T>,
 }
-type NodeChild<T: Ord> = Option<NonNull<Node<T>>>;
-impl<T: Ord> Node<T>{
+type NodeChild<T> = Option<NonNull<Node<T>>>;
+impl<T> Node<T>{
     pub fn new(entry: T) -> Self{
         Self{entry, next: None}
     }
 }
 #[derive(Debug)]
-struct Queue<T: Ord>{
+struct Queue<T>{
     front: NodeChild<T>,
     back: NodeChild<T>
 }
-impl<T: Ord + std::fmt::Display> Queue<T>{
+impl<T> Queue<T>{
     pub fn new() -> Self{
         Self{front: None, back: None}
     }
@@ -55,5 +55,33 @@ impl<T: Ord + std::fmt::Display> Queue<T>{
     }
     pub fn peek_front(&self) -> Option<&T>{
         unsafe{self.front.map(|node| &(*node.as_ptr()).entry)}
+    }
+}
+impl<T> Drop for Queue<T>{
+    fn drop(&mut self) {
+        while self.dequeue().is_some() {}
+    }
+}
+
+pub struct IntoIter<T>(Queue<T>);
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.dequeue()
+    }
+}
+impl<T> IntoIterator for Queue<T> {
+    type Item = T;
+    type IntoIter = IntoIter<T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        IntoIter(self)
+    }
+}
+impl<T> FromIterator<T> for Queue<T> {
+    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
+        let mut queue = Queue::new();
+        for item in iter {queue.enqueue(item);}
+        queue
     }
 }
