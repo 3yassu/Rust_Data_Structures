d3543f08e3dafd288d0f6c2f742d3ce7ea0cf019
3yassu <68251430+3yassu@users.noreply.github.com>
2025-03-31 23:38:43 -0500

    Continue working on doublyLinkedList

--- LinkedList/doublyLinkedList.rs
+++ LinkedList/doublyLinkedList.rs
@@ -1,7 +1,13 @@
 use std::cmp::Ordering;
 use std::NonNull;
+use core::marker::PhantomData;
 struct Node<T: Ord>{
     entry: T,
     next: NodeChild<T>
 }
-struct NodeChild<T>(Option<NonNull<Node<T>>>);
+type NodeChild<T> = Option<NonNull<Node<T>>>;
+struct LinkedList<T>{
+    front: NodeChild<T>,
+    back: NodeChild<T>,
+    _boo: PhantomData<T>
+}
